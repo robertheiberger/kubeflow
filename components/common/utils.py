a256ca822fca82d42808145e4a4abdb3fdff14d5
robertheiberger <robert.heiberger@gmail.com>
2019-09-12 22:51:36 +0000

    Checking in.

--- components/common/utils.py
+++ components/common/utils.py
@@ -18,13 +18,31 @@ import time
 
 import boto3
 from botocore.exceptions import ClientError
+from botocore.utils import InstanceMetadataFetcher
+from botocore.credentials import InstanceMetadataProvider
+
 import json
 from common.S3Url import S3Url
+from pyathena import connect
+import pandas as pd
+
+def get_creds():
+    provider = InstanceMetadataProvider(iam_role_fetcher=InstanceMetadataFetcher(timeout=1000, num_attempts=2))
+    creds = provider.load()
+    return creds;
+
+def get_session():
+    creds = get_creds()
+    session = boto3.Session(
+        aws_access_key_id = creds.access_key,
+        aws_secret_access_key = creds.secret_key,
+        aws_session_id = creds.token)
+    return session
 
 def s3_get_file(file_name, input_path):
     
     s3f = S3Url(file_name)
-    s3 = boto3.client('s3')
+    s3 = get_session().client('s3')
     
     if not os.path.exists(input_path):
         os.makedirs(input_path)
@@ -44,11 +62,23 @@ def decrypt_file(file_name, kms_key):
     
 def s3_upload_file(s3_location, local_file):
 
-    s3 = boto3.client('s3')
+    s3 = get_session().client('s3')
     
     s3f = S3Url(s3_location)
     
     s3.upload_file(local_file, s3f.bucket, s3f.key)
 
     
+    
+def athena_get_file():
+
+    creds = get_creds()
+    
+    conn = connect(
+        aws_access_key_id = creds.access_key,
+        aws_secret_access_key = creds.secret_key,
+        s3_staging_dir='s3://YOUR_S3_BUCKET/path/to/',
+        region_name='us-west-2')
+    
+    df = pd.read_sql("SELECT * FROM many_rows", conn)
     
